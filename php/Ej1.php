95b5853b139b59e208beb72258f84bc35f53e0a0
josuepl <josue.perez.lucero@gmail.com>
2018-06-11 11:08:41 -0500

    Add files via upload

---
+++ php/Ej1.php
@@ -0,0 +1,14 @@
+<html>
+</head>
+<body>
+<?php echo 'Mientras que esto va a ser interpretado.'; ?>
+
+<?php
+
+$ini = "Hola ";
+$fin = " a todos";
+$todo = $ini.$fin;
+echo $todo;
+?>
+</body>
+</html>
---
+++ php/conexion/conexion.php
@@ -0,0 +1,5 @@
+<?php
+    $usuario = 'admin';
+    $contrasena = "DB4dm1n@";
+    $mbd = new PDO('mysql:host=localhost;dbname=ejemplo', $usuario, $contrasena);
+?>
