a69ddb6cf474887961dfab7ab2b08df12acac746
josuepl <josue.perez.lucero@gmail.com>
2018-06-12 21:21:21 -0500

    Add files via upload

---
+++ php/ses2.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+if (empty($_SESSION['count'])) {
+   $_SESSION['count'] = 1;
+} else {
+   $_SESSION['count']++;
+}
+?>
+
+<p>
+Hola visitante, ha visto esta página <?php echo $_SESSION['count']; ?> veces.
+</p>
+
+<p>
+Para continuar, <a href="ses3.php?">haga clic
+aquí</a>.
+</p>
